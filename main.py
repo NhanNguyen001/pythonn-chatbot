94d0b866cf047ce616639545f6cb8645cf4a5a07
Nhan Nguyen <nhan2151@gmail.com>
2025-01-12 15:52:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from openai import OpenAI
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 
 if __name__ == "__main__":
     openai = OpenAI()
     response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{
-            "role": "system",
-            "content": "You are a helpful assistant."}]
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are the CEO of Apple."},
+            # {
+            #     "role": "assistant",
+            #     "content": "The Spurs won the 2005 NBA Championship.",
+            # },
+            {
+                "role": "user",
+                "content": "Who was on the team?",
+            },
+        ],
     )
+    # response.choices[0].message.content
 
-    print(response)
+    print(response.choices[0].message.content)
