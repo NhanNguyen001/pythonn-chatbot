ad20d9fe4da96e5718dc300b19b64c6bddc42615
Nhan Nguyen <nhan2151@gmail.com>
2025-01-12 20:29:42 +0700

    Implement API endpoint

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+from typing import Annotated
+
 from dotenv import load_dotenv
+from fastapi import FastAPI, Form
 from openai import OpenAI
 
 load_dotenv()
 
-if __name__ == "__main__":
-    openai = OpenAI()
-    chat_log = []
-    while True:
-        user_input = input()
-        if user_input.lower() == "stop":
-            break
+app = FastAPI()
 
-        chat_log.append({"role": "user", "content": user_input})
+chat_log = []
 
-        response = openai.chat.completions.create(
-            model="gpt-4o",
-            messages=chat_log,
-            temperature=0.6,
-        )
+client = OpenAI()
 
-        bot_response = response.choices[0].message.content
 
-        chat_log.append({"role": "assistant", "content": bot_response})
+@app.post("/")
+async def chat(user_input: Annotated[str, Form()]):
+    chat_log.append({"role": "user", "content": user_input})
 
-        print(bot_response)
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=chat_log,
+        temperature=0.6,
+    )
+
+    bot_response = response.choices[0].message.content
+
+    chat_log.append({"role": "assistant", "content": bot_response})
+
+    return bot_response
